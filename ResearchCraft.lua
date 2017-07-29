092d1c29b6257a3a193a4e4036847f7ad030f181
silvereyes <silvereyes333@gmail.com>
2017-07-28 22:20:16 -0500

    Version 1.3.1
    - Update 15 Horns of the Reach compatibility

--- ResearchCraft.lua
+++ ResearchCraft.lua
@@ -1,7 +1,7 @@
 ResearchCraft = {
     name = "ResearchCraft",
     title = "Research Craft",
-    version = "1.3.0",
+    version = "1.3.1",
     author = "|c99CCEFsilvereyes|r",
     defaults = {
         reserve = 20,
